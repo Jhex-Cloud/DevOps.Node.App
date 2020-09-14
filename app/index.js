ca45af72340a679f8e10821ffc69116fb4f07352
JaganKaartik <kaartikjagan@gmail.com>
2020-09-15 03:17:51 +0530

    Minor typo

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5050
+
+app.get('/',(req,res) => { res.send('Hello World') })
+
+app.listen(port,()=>{ console.log(`App running at port: ${port}`) })
+
+
