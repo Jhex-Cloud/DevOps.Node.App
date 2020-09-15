4b9e155ff48a6240cf60bd2576e6085279dcba85
JaganKaartik <kaartikjagan@gmail.com>
2020-09-15 16:12:06 +0530

    Added APIs Routes

--- app/Routes/api.js
+++ app/Routes/api.js
@@ -1,7 +1,64 @@
 const express = require('express')
 const router = express.Router()
-const recepies = require('../Database/')
+const recepies = require('../Database/recepies')
 
+const idFilter = req => recepie => recepie.id === parseInt(req.params.id);
+
+//Return all recepies as JSON
 router.get('/all', (req, res) => res.json(recepies))
+
+//Return Single recepie
+router.get('/:id', (req, res) => {
+    const found = recepies.some(idFilter(req))
+    if (found) {
+        res.json(recepies.filter(idFilter(req)));
+    } else {
+        res.status(400).json({ msg: `No recepie with the id of ${req.params.id}` });
+    }
+})
+
+// Create Recepie
+router.post('/add', (req, res) => {
+    const newRecepie = {
+        ...req.body
+    };
+    if (!newRecepie.id || !newRecepie.title || !newRecepie.href || !newRecepie.ingredients) {
+        return res.status(400).json({ msg: 'Pls include ID.. Title.. Href.. Ingredients' });
+    }
+    recepies.push(newRecepie);
+    res.json(recepies);
+});
+
+// Update Recepie Info
+router.put('/:id', (req, res) => {
+    const found = recepies.some(idFilter(req));
+
+    if (found) {
+        recepies.forEach((recepie, i) => {
+            if (idFilter(req)(recepie)) {
+
+                const updrecepie = { ...recepie, ...req.body };
+                recepies[i] = updrecepie
+                res.json({ msg: 'recepie updated', updrecepie });
+            }
+        });
+    } else {
+        res.status(400).json({ msg: `No recepie with the id of ${req.params.id}` });
+    }
+});
+
+// Delete recepie
+router.delete('/:id', (req, res) => {
+    const found = recepies.some(idFilter(req));
+
+    if (found) {
+        res.json({
+            msg: 'recepie deleted',
+            recepies: recepies.filter(recepie => !idFilter(req)(recepie))
+        });
+    } else {
+        res.status(400).json({ msg: `No recepie with the id of ${req.params.id}` });
+    }
+});
 
 module.exports = router
--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 5000
+const apiRoutes = require('./Routes/api')
 
-app.get('/',(req,res) => { res.send('Hello World') })
+app.get('/', (req, res) => { res.send('Go to /api to access the static api') })
+app.use('/api', apiRoutes)
 
-app.listen(port,()=>{ console.log(`App running at port: ${port}`) })
+app.listen(port, () => { console.log(`App running at port: ${port}`) })
 
 
