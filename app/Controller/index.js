778d159efded2e7c70bdf7e52d39937de393109c
JaganKaartik <kaartikjagan@gmail.com>
2020-10-08 23:30:48 +0530

    Migrated to TS

--- app/Controller/index.js
+++
@@ -1,3 +0,0 @@
-module.exports.RootController = (req, res) => {
-    res.send(`<h1> Head over to /api to access the demo api </h1>`)
-}
---
+++ app/Controller/index.ts
@@ -0,0 +1,5 @@
+const RootController = (req, res) => {
+  res.send(`<h1> Head over to /api to access the demo api </h1>`)
+}
+
+export = RootController
