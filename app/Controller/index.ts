aa3801b021a22a7f0b0569c5938b197082b40d61
JaganKaartik <kaartikjagan@gmail.com>
2020-10-09 22:22:14 +0530

    Added EJS Support. Fixed Lint Script

--- app/Controller/index.ts
+++ app/Controller/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const RootController = (req: express.Request, res: express.Response) => {
-  res.send('<h1> Head over to /api to access the demo api </h1>')
+  res.render('pages/index')
 }
 
 export = RootController
