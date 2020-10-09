0f8c3a26ad2637c189f3031ad880be795ff64c9a
JaganKaartik <kaartikjagan@gmail.com>
2020-10-09 23:03:17 +0530

    Fixed EJS- Views path

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import RootController from './Controller'
 
+const path = require('path')
+
 const app = express()
 const port = process.env.PORT || 5000
 const apiRoutes = require('./Routes/api')
 
+app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
 app.get('/', RootController)
 
